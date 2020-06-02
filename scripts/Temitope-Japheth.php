c3f986e0cddc5bf63e7121df5fda7a3a6650e38e
Japhethtemitope <66273534+Japhethtemitope@users.noreply.github.com>
2020-06-03 00:25:34 +0100

    Update Temitope-Japheth.php

--- scripts/Temitope-Japheth.php
+++ scripts/Temitope-Japheth.php
@@ -3,9 +3,9 @@ $txt1 = "Temitope Japheth";
 $txt2 = "HNG-04757";
 $txt3 = "PHP";
 
-echo " Hello, This is  $txt1  ";
-echo "with HNGi7 ID: $txt2 using ";
-echo $txt3 . " ";
-echo "for stage 2 task" . "<br>";
-echo "Thank you";
+echo (" Hello, This is  $txt1  ");
+echo ("with HNGi7 ID: $txt2 using ");
+echo ($txt3 . " ");
+echo ("for stage 2 task" . "<br>");
+echo ("Thank you");
 ?>
