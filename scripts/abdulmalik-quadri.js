b176b48a35e656e8e225fbcf4d3d81254bb29b55
Quadrizz <mackquadrizz@gmail.com>
2020-06-03 14:20:46 +0100

    initial commit

---
+++ scripts/abdulmalik-quadri.js
@@ -0,0 +1,5 @@
+function hng(name, id, language){
+    console.log(`Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`)
+}
+
+hng("Quadri Abdulmalik", "HNG-00837", "JavaScript", )
