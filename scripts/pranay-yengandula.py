00fdee3aa64556767361dba8be6e3cea120d0a8b
benlex5 <46024202+benlex5@users.noreply.github.com>
2020-06-03 22:01:03 +0100

    Fork and updated to Master

--- scripts/pranay-yengandula.py
+++ scripts/pranay-yengandula.py
@@ -1,4 +1,7 @@
-name="Pranay Yenagandula"
-ID="HNG-01542"
-language="Python"
-print("Hello World, this is",name,"with HNGi7 ID ",ID,"using",language,"for stage 2 task")
+#Pranay-Yenagandula 
+
+class display_text:
+    def __init__(self):
+        print("Hello World, this is Pranay Yenagandula with HNGi7 ID HNG-01542 and email pranay41@gmail.com using Python for stage 2 task")
+
+d=display_text()
