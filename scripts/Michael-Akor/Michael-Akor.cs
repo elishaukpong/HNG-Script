c80f1ce4bd03f690f571536178a7ac7bba512a53
Akor Michael <62037109+aim-bits@users.noreply.github.com>
2020-06-03 12:00:08 +0100

    Michael-Akor

---
+++ scripts/Michael-Akor/Michael-Akor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Michael_Akor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World. this is Akor Michael with HNGi7 ID HNG-00961 using c# for stage 2 task");
+        }
+    }
+}
