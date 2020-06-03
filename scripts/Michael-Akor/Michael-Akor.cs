c60d47af8b7d67593270d82770e1a8ccc7c439ae
Akor Michael <62037109+aim-bits@users.noreply.github.com>
2020-06-03 12:35:20 +0100

    Updated email for Michael-Akor

--- scripts/Michael-Akor/Michael-Akor.cs
+++ scripts/Michael-Akor/Michael-Akor.cs
@@ -6,7 +6,7 @@ namespace Michael_Akor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World. this is Akor Michael with HNGi7 ID HNG-00961 using c# for stage 2 task");
+            Console.WriteLine("Hello World. this is Akor Michael with HNGi7 ID HNG-00961 and email michaelaim60@gmail.com using c# for stage 2 task");
         }
     }
 }
