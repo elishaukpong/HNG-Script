a5fdbe0b215f11522092f425f31b83bcc3ae1e22
Keicee32 <kelss451@gmail.com>
2020-06-03 13:59:55 -0700

    f removed and email updated

--- scripts/egekenze-kelechi.py
+++ scripts/egekenze-kelechi.py
@@ -1,1 +1,1 @@
-print(f"Hello World, this is Egekenze Kelechi with HNGi7 ID HNG-02308 and email kelss451@gmail.com using Python for stage 2 task")
+print("Hello World, this is Egekenze Kelechi with HNGi7 ID HNG-02308 and email kelss451@gmail.com using Python for stage 2 task")
