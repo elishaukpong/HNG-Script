67665f4157aabb6a29d33737babe93c4a637db1f
Queencobham <34819064+Queencobham@users.noreply.github.com>
2020-06-03 01:56:54 +0100

    Delete Queen-cobham.js

--- scripts/Queen-cobham.js
+++
@@ -1,8 +0,0 @@
-<script>
-    function getInfo(name, hngi7Id, language ){
-      var result = "";
-      result += '"Hello World, this is ' + name + ' with HNGi7 ID ' + hngi7Id + ' using ' + language + ' for stage 2 task".'
-      return result;
-    }
-  console.log(getInfo("Queen Cobham", "HNG-02694", "javascript"));   
-  </script>
