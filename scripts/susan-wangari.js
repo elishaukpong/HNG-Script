9159dbd63cb07396b024e8ae0f32a0a2f4f8489f
coder girl <56164343+kareey-commits@users.noreply.github.com>
2020-06-04 11:33:27 +0300

    made changes to my hng id

--- scripts/susan-wangari.js
+++ scripts/susan-wangari.js
@@ -6,5 +6,5 @@ function showContent(firstName,lastName,id,emailAddress,language){
     this.emailAddress = emailAddress;
     this.language = language;
 }
-content = new showContent("Susan","Wangari","02129","susanwangari810@gmail.com","JavaScript")
+content = new showContent("Susan","Wangari","HNG-02129","susanwangari810@gmail.com","JavaScript")
 console.log("Hello World,this is " + content.firstName + " " + content.lastName + " with HNGi7 ID " + content.id + " " + "and" + " " + content.emailAddress + " " + "using" +" "+ content.language + " for stage 2 task");
