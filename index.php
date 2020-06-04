f5debd8712b5a3f5bd1007393b82f0289ba9416f
fidele000 <fihacker000@gmail.com>
2020-06-04 16:50:49 +0300

    modification of index.php

--- index.php
+++ index.php
@@ -14,27 +14,74 @@ $output = [];
 $outputJSON = [];
 $data = [];
 
+foreach ($files as $file) {
+
+    $extension = explode('.', $file);
+
+    switch (@$extension[1]) {
+        case 'php':
+            $startScript = "php";
+            break;
+        case 'js':
+            $startScript = "node";
+            break;
+        case 'py':
+            $startScript = "python";
+            break;
+        case 'dart':
+            $startScript = "dart";
+            break;
+        case 'java':
+            $startScript = "java";
+
+            exec("javac scripts/" . $file);
+            break;
+
+        default:
+            $startScript = "php";
+            break;
+    }
+
+    $f = @exec($startScript . " scripts/" . $file);
+
+
+    $newString = str_ireplace(getEmailFromFileContent($f), ' ', str_ireplace('and email', ' ', $f));
+    $regexReturn  = testFileContent($f);
+
+    $data[] = [
+        'file' => $file,
+        'output' => $newString,
+        'name' => str_replace('-', ' ', $extension[0]),
+        'id' => $regexReturn[1],
+        'email' => trim(getEmailFromFileContent($f)),
+        'language' => $regexReturn[2],
+        'status' => $regexReturn[0],
+    ];
+    $outputJSON = $data;
+
+    @$output[] = [$newString, testFileContent($f), str_replace('-',' ',$extension[0]), trim(getEmailFromFileContent($f))];
+}
+
 function testFileContent($string)
 {
     if (preg_match('/^Hello\sWorld[,|.|!]*\sthis\sis\s([a-zA-Z|-]{2,}\s){1,6}with\sHNGi7\sID\s(HNG-\d{3,})\sand\semail\s{1,3}(([\w+\.\-]+)@([\w+\.\-]+)\.([a-zA-Z]{2,5}))\s{1,3}using\s([a-zA-Z|#]{2,})\sfor\sstage\s2\stask.?$/i', trim($string), $values)) {
-        return ['pass',$values[2],$values[7]];
+        return ['pass', $values[2], $values[7]];
     }
 
-    return ['fail',null,null];
+    return ['fail', null, null];
 }
 
 function getEmailFromFileContent($string)
 {
-    preg_match('/\s?(([\w+\.\-]+)@([\w+\.\-]+)\.([a-zA-Z]{2,5}))/i', trim($string) , $matches, PREG_OFFSET_CAPTURE);
+    preg_match('/\s?(([\w+\.\-]+)@([\w+\.\-]+)\.([a-zA-Z]{2,5}))/i', trim($string), $matches, PREG_OFFSET_CAPTURE);
 
     return @$matches[0][0];
 }
 
 
 if (isset($json) && $json == 'json') {
-    header('Content-type: application/json');
-
-}else{
+    echo json_encode($outputJSON);
+} else {
     if (ob_get_level() == 0) ob_start();
 ?>
     <html>
@@ -44,192 +91,89 @@ if (isset($json) && $json == 'json') {
     </head>
 
     <body>
-    <div class="container-fluid">
-        <nav class="navbar navbar-dark bg-dark fixed-top">
-                    <span class="navbar-text">
-                        HNGi7 Team Sentry
-                    </span>
-            <div class="float-right text-white">
-                <small>
-                    Leader: <span class="btn btn-sm btn btn-outline-primary">@E.U</span>
-                </small> &nbsp;
-                <small>
-                    FrontEnd: <span class="btn btn-sm btn btn-outline-success">@dona</span>
-                </small> &nbsp;
-                <small>
-                    DevOps: <span class="btn btn-sm btn btn-outline-info">@Fidele</span>
-                </small> &nbsp;
+        <div class="container-fluid">
+            <nav class="navbar navbar-dark bg-dark fixed-top">
+                <span class="navbar-text">
+                    HNGi7 Team Sentry
+                </span>
+                <div class="float-right text-white">
+                    <small>
+                        Leader: <span class="btn btn-sm btn btn-outline-primary">@E.U</span>
+                    </small> &nbsp;
+                    <small>
+                        FrontEnd: <span class="btn btn-sm btn btn-outline-success">@dona</span>
+                    </small> &nbsp;
+                    <small>
+                        DevOps: <span class="btn btn-sm btn btn-outline-info">@Fidele</span>
+                    </small> &nbsp;
+                </div>
+            </nav>
+        </div>
+        <div class="container">
+            <div class="row" style="padding: 2em 0" class="text-center">
+
             </div>
-        </nav>
-    </div>
-    <div class="container">
-        <div class="row" style="padding: 2em 0" class="text-center">
+            <table class="table table-hover center table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Message</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $row = 1;
+                    foreach ($output as $out) {
+
+                        $status = $out[1][0] == 'Pass' ? 1 : 0;
+                        $email = 'No Email';
+                        $statuses = $out[1][0];
+
+                        if (isset($out[3]) && !empty($out[3])) {
+                            $email = $out[3];
+                        }
+                        if ($statuses=='pass') {
+
+                    ?>
+                            <tr class="table-success">
+                                <th scope="row"><?php echo $row; ?></th>
+                                <td><b><?php echo $out[2]; ?></b></td>
+                                <td><?php echo $out[0] ?></td>
+                                <td>
+                                    <?php echo $email; ?>
+                                </td>
+                                <td><?php echo $statuses; ?> ✅</td>
+                            </tr>
+                        <?php
+                        } else {
+                        ?>
+                            <tr class="table-danger">
+                                <th scope="row"><?php echo $row; ?></th>
+                                <td><b><?php echo $out[2]; ?></b></td>
+                                <td><?php echo $out[0] ?></td>
+                                <td>
+                                    <?php echo $email; ?>
+                                </td>
+                                <td><?php echo $statuses; ?> ❌</td>
+                            </tr />
+                    <?php
+                        }
+                        $row++;
+                        ob_flush();
+                        flush();
+                    }
+                    ?>
+                </tbody>
+            </table>
+
 
         </div>
-        <table class="table table-hover center table-striped">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Message</th>
-                <th scope="col">Email</th>
-                <th scope="col">Status</th>
-            </tr>
-            </thead>
-            <tbody>
-
-            <?php
-            $row = 1;
-
-            foreach ($files as $file) {
-
-                $extension = explode('.', $file);
-
-                switch (@$extension[1]) {
-                    case 'php':
-                        $startScript = "php";
-                        break;
-                    case 'js':
-                        $startScript = "node";
-                        break;
-                    case 'py':
-                        $startScript = "python";
-                        break;
-                    case 'dart':
-                        $startScript = "dart";
-                        break;
-                    case 'java':
-                        $startScript = "java";
-
-                        exec("javac scripts/" . $file);
-                        break;
-
-                    default:
-                        $startScript = "php";
-                        break;
-                }
-
-                $f = @exec($startScript . " scripts/" . $file);
-
-
-                $newString = str_ireplace(getEmailFromFileContent($f),' ', str_ireplace('and email',' ', $f));
-                $regexReturn  = testFileContent($f);
-
-                $data[] = [
-                    'file' => $file,
-                    'output' => $newString,
-                    'name' => str_replace('-',' ',$extension[0]),
-                    'id' => $regexReturn[1],
-                    'email' => trim(getEmailFromFileContent($f)),
-                    'language' => $regexReturn[2],
-                    'status' => $regexReturn[0],
-                ];
-
-                    $testEmailVariable = trim(getEmailFromFileContent($f));
-                    $status = testFileContent($f)[0];
-                    $email = 'No Email';
-                    $name = str_replace('-',' ',$extension[0]);
-
-                    if(isset($testEmailVariable) && !empty($testEmailVariable)){
-                        $email = $testEmailVariable;
-                    }
-
-                    if ($status == 'pass') {
-
-                        echo <<<EOL
-                                <tr class="table-success">
-                                <th scope="row">$row</th>
-                                <td><b>$name</b></td>
-                                <td>$newString</td>
-                                <td>
-                                    $email
-                                </td>
-                                <td>$status ✅</td>
-                                </tr>
-                             EOL;
-                    }
-                    else {
-                        echo <<<EOL
-                                <tr class="table-danger">
-                                <th scope="row">$row</th>
-                                 <td><b>$name</b></td>
-                                <td>$newString</td>
-                                <td>
-                                    $email
-                                </td>
-                                <td>$status ❌</td>
-                                </tr>
-                            EOL;
-                    }
-                    $row++;
-
-                    ob_flush();
-                    flush();
-            }
-            ?>
-
-            </tbody>
-        </table>
-
-
-    </div>
 
     </body>
 
     </html>
 <?php
 }
-
-if (isset($json) && $json == 'json') {
-
-    foreach ($files as $file) {
-
-        $extension = explode('.', $file);
-
-        switch (@$extension[1]) {
-            case 'php':
-                $startScript = "php";
-                break;
-            case 'js':
-                $startScript = "node";
-                break;
-            case 'py':
-                $startScript = "python";
-                break;
-            case 'dart':
-                $startScript = "dart";
-                break;
-            case 'java':
-                $startScript = "java";
-
-                exec("javac scripts/" . $file);
-                break;
-
-            default:
-                $startScript = "php";
-                break;
-        }
-
-        $f = @exec($startScript . " scripts/" . $file);
-
-
-        $newString = str_ireplace(getEmailFromFileContent($f),' ', str_ireplace('and email',' ', $f));
-        $regexReturn  = testFileContent($f);
-
-        $data[] = [
-            'file' => $file,
-            'output' => $newString,
-            'name' => str_replace('-',' ',$extension[0]),
-            'id' => $regexReturn[1],
-            'email' => trim(getEmailFromFileContent($f)),
-            'language' => $regexReturn[2],
-            'status' => $regexReturn[0],
-        ];
-        $outputJSON = $data;
-
-    }
-
-
-    echo json_encode($outputJSON);
-}
-?>
