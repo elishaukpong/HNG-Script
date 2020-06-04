ada8c83cd317543735b2778b7960f4c37b48547c
starday <starday01@yahoo.com>
2020-06-04 07:41:46 +0100

    Updated code to use variables and string interpolation

--- scripts/sunday-orimoyegun.js
+++ scripts/sunday-orimoyegun.js
@@ -1,3 +1,6 @@
-﻿console.log(
-    'Hello World, this is Orimoyegun Sunday with HNGi7 ID HNG-01400 and email orimoyegun.sunday@gmail.com using Javascript for stage 2 task'
-);
+﻿let _fullName = 'Orimoyegun Sunday';
+let _hngId = 'HNG-01400';
+let _language = 'Javascript';
+let _email = 'orimoyegun.sunday@gmail.com'
+
+console.log(`Hello World, this is ${_fullName} with HNGi7 ID ${_hngId} and email ${_email} using ${_language} for stage 2 task`);
