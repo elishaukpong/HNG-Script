c9df6859225ca8095d885361dfed086b1e564511
starday01 <33865418+starday01@users.noreply.github.com>
2020-06-04 07:45:09 +0100

    Update sunday-orimoyegun.js

--- scripts/sunday-orimoyegun.js
+++ scripts/sunday-orimoyegun.js
@@ -1,6 +1,6 @@
 ﻿let _fullName = 'Orimoyegun Sunday';
 let _hngId = 'HNG-01400';
 let _language = 'Javascript';
-let _email = 'orimoyegun.sunday@gmail.com'
+let _email = 'orimoyegun.sunday@gmail.com';
 
 console.log(`Hello World, this is ${_fullName} with HNGi7 ID ${_hngId} and email ${_email} using ${_language} for stage 2 task`);
