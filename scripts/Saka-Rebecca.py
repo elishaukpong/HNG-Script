9fb6c3cf661e90ed185cf3ccef3f1ef28cca549e
Becca-Saka <Ree4sure@gmail.com>
2020-06-04 19:33:22 +0100

    Update

--- scripts/Saka-Rebecca.py
+++ scripts/Saka-Rebecca.py
@@ -9,7 +9,7 @@ Id = "HNG-05990"
 #Function Definition
 
 def TaskDef():
-    print("Hello world, this is",Name,"With HNGi7 ID",Id,"and email",Email,"using python for stage 2 task")
+    print("Hello world, this is "+Name+" With HNGi7 ID "+Id+" and email "+Email+" using python for stage 2 task")
 
 
 #Function Call
