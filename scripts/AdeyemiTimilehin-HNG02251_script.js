fad1efbb3382ac33818bdd371208ad24f41a8aaf
Adeyemi Timilehin <adeyemitimmy@gmail.com>
2020-06-04 04:08:11 +0100

    Please help me merge

---
+++ scripts/AdeyemiTimilehin-HNG02251_script.js
@@ -0,0 +1,5 @@
+const name="Adeyemi Timilehin Mike";
+const email="adeyemitimmy@gmail.com";
+const language="JavaScript";
+const Id="02251";
+console.log(`Hello world,this is ${name} with HNG i7 ID ${Id} using ${language}
