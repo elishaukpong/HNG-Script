9f804ea78669ae7ae62ca96e79c5ef99ab6228a4
Kasopefoluwa Abolade <aboladekasope@gmail.com>
2020-06-04 11:07:59 +0100

    Updated email to pass test

--- scripts/abolade-kasope.js
+++ scripts/abolade-kasope.js
@@ -3,5 +3,6 @@
 const fullname = 'Abolade Kasope'
 const id = ': HNG-06623'
 const lang = 'Javascript'
+const myMail = 'aboladekasope@gmail.com'
 
-console.log (`Hello World, this is ${fullname} with HNGi7 ID${id} using ${lang} for stage 2 task`)
+console.log (`Hello World, this is ${fullname} and email ${myMail} with HNGi7 ID${id} using ${lang} for stage 2 task`)
