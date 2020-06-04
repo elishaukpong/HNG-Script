662e34fc740ec1a95071dc1a61e3f7d77148b0b7
Eddiesavvy <edwinokhai@gmail.com>
2020-06-04 10:35:05 +0100

    Added to script

--- scripts/Edwin.Ebikhumi.js
+++ scripts/Edwin.Ebikhumi.js
@@ -1,6 +1,6 @@
 var fullname = "Edwin Ebikhumi";
-var id = "HNG-03314"; 
+var id = "HNG-03314";
 var language = "JavaScript";
-var message = "Hello world, this is " + fullname + " with HNGi7 ID " + id + " using " + language + " for stage 2 task."
- 
-console.log('message');
+var email = "edwinokhai@gmail.com"
+var message = `Hello World, This is ${fullname} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`
+console.log(message);
