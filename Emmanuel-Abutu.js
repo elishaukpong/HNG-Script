abcb24ec1e9bd30b951ffbf12a9ac24fe2759b3e
Abutue10 <66272252+Abutue10@users.noreply.github.com>
2020-06-04 11:08:42 +0100

    Emmanuel-Abutu/script

---
+++ Emmanuel-Abutu.js
@@ -0,0 +1,11 @@
+// asset/js/my_javascript
+2
+3
+4 // print message on the webpage
+5 document.write("Hello World, This is Emmanuel Abutu with HNGI.7 ID HNG-03705 using JavaScript for stage 2 task <br>");
+6
+7
+8 // variable example
+9 var email address = "abutue10@gmail.com";
+10 
+11 document.getElementById("p_name").innerHTML= "Hello "+ email address + "<br>;
