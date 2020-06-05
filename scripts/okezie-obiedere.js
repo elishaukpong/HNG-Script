487b0e39e4dc58e73ecd9df622076fd17bda4aab
okezieobi <obiedereokezie@gmail.com>
2020-06-05 15:03:30 +0100

    Fixed output

--- scripts/okezie-obiedere.js
+++ scripts/okezie-obiedere.js
@@ -1,1 +1,9 @@
-Hello World, this is Frank Okezie Obiedere with HNGi7 ID HNG-05247 and email obiedereokezie@gmail.com using JavaScript for stage 2 task
+const profile = {
+    name: 'Frank Okezie Obiedere',
+    id: 'HNG-05247',
+    email: 'obiedereokezie@gmail.com',
+}
+
+const output = `Hello World, this is ${profile.name} with HNGi7 ID ${profile.id} and email ${profile.email} using JavaScript for stage 2 task`
+
+console.log(output);
