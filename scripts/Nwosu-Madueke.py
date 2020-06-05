39690547bea3dbeb15a1d081950c77b820b4c999
Jackson Jonah <jackieriel14@gmail.com>
2020-06-05 18:33:46 +0100

    Create Nwosu-Madueke.py
    Updated script

---
+++ scripts/Nwosu-Madueke.py
@@ -0,0 +1,5 @@
+fullname = 'Nwosu-Madueke Christopher'
+ID = 'HNG-06465'
+language = 'python'
+text = 'Hello world, this is '+fullname+' with HNGi7 ID '+ID+' using '+language+ ' for stage 2 task.'
+print (text)
