3c83a57abf8e99025bf6cfd2b88e19de3cdb7eb6
davidadediji <davidadediji@gmail.com>
2020-06-05 20:24:41 +0100

    updated extension

--- scripts/david-adediji.py
+++ scripts/david-adediji.py
@@ -1,1 +1,6 @@
-print('Hello World, this is {Fullname} with HNGi7 ID {ID} and email {Email} using {Language} for stage 2 task'.format(Fullname='David Adediji', ID='HNG-05328',Email='davidadediji@gmail.com', Language='Python' ))
+Fullname='David Adediji'
+ID='HNG-05328'
+Email='davidadediji@gmail.com'
+Language='Python'
+
+print ('Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task'.format(Fullname,ID,Email,Language))
