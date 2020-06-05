a229e877f1bf1aa3fdd1b958f55e50896be782c5
davidadediji <davidadediji@gmail.com>
2020-06-05 10:03:25 +0100

    updated davidadediji

--- scripts/david-adediji.py
+++ scripts/david-adediji.py
@@ -1,6 +1,6 @@
-fullnane='David Adediji'
-ID='HNG-05328'
-email='davidadediji@gmail.com
+fullname='David Adediji'
+ID='HNG-0528'
+email='davidadediji@gmail.com'
 language='Python'
 
-print(f'Hello World, this is {fullname} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task.')
+print(f'Hello World, this is {fullname} with HNGi7{ID} and email {email} using {language} for stsge 2 tasks')
