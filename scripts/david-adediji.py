70214b2f83050b329a006744ae0abe6a6ec081a2
davidadediji <davidadediji@gmail.com>
2020-06-05 09:35:39 +0100

    new extension david-adediji script

---
+++ scripts/david-adediji.py
@@ -0,0 +1,6 @@
+fullnane='David Adediji'
+ID='HNG-05328'
+email='davidadediji@gmail.com
+language='Python'
+
+print(f'Hello World, this is {fullname} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task.')
