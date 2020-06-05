a35da509e044cd808bfb0c18bb02d2b81449a3e9
chidubem-max <60617530+chidubem-max@users.noreply.github.com>
2020-06-05 19:58:43 +0100

    Update Chidubem-Onyeukwu.js

--- Chidubem-Onyeukwu.js
+++ Chidubem-Onyeukwu.js
@@ -1,9 +1,10 @@
 let fullname = "Chidubem Onyeukwu";
 let id = "HNG-03733";
+let email = "onyeukwuchidubemclinton@gmailcom";
 let language = "Javascript";
 
 function message() {
- const message = "Hello World, this is" + fullname +" with HNGi7 ID" + id + "using" + language + " for stage 2 task ";
+ const message = " Hello World, this is " + fullname + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task " ;
  
  return message;
 
