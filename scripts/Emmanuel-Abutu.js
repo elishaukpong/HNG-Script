c525264a2f547d4c9eea08ccf295537c95c01379
Abutue10 <66272252+Abutue10@users.noreply.github.com>
2020-06-06 18:02:46 +0100

    Updated Emmanuel-abutu.js

--- scripts/Emmanuel-Abutu.js
+++ scripts/Emmanuel-Abutu.js
@@ -2,5 +2,8 @@ var name = "Emmanuel Abutu";
 var ID = "HNG-03705";
 var language = "Javascript";
 var email = "abutue10@gmail.com";
+let v = `Hello World, this is ${name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`);
+let outputData = `Hello World, this is ${name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
+
+console.log(outputData);
