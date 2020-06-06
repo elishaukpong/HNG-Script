6d050039521db477204e71d249b98e58c8040e9d
Bankole Ahmed <kidrolex19@gmail.com>
2020-06-06 01:32:49 +0100

    update ahmed-bankole.js to fit requirements

--- scripts/Ahmed-Bankole.js
+++ scripts/Ahmed-Bankole.js
@@ -1,6 +1,7 @@
-    var fullname = "Bankole Ahmed";
-    var id = "HNG-02467";
-    var language = "JavaScript";
-    var email = "kidrolex19@gmail.com"; 
-
-    console.log("Hello World, this is " +  fullname + " " + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task");
+const hng = { 
+    name: 'Ahmed BBankole',
+    id: 'HNG-06260', 
+    language:'JavaScript',
+    email:'kidrolex19@gmail.com'
+};
+console.log (`Hello World, this is ${hng.name} with HNGi7 ID ${hng.id} and email ${hng.email} using ${hng.language} for stage 2 task`);
