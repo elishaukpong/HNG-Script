029e055cf45565127c6111dc4b6a0ed54b998caf
sultanbello <tinetbox@gmail.com>
2020-06-06 07:29:28 +0100

    Added Task 2 file

---
+++ scripts/Alake-Tobi.py
@@ -0,0 +1,6 @@
+fn = "Alake Tobi"
+mail = "tinetbox@gmail.com"
+HNGi7_id = "HNG-05970"
+lang = "Python"
+
+print ("Hello World, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(fn, HNGi7_id, mail, lang))
