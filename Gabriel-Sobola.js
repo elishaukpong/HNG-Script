62ea7370c28d9060cb7de3a15d9cfbe3283e58c8
Gabrinas <65027184+Gabrinas@users.noreply.github.com>
2020-06-07 15:13:07 +0100

    Update Gabriel-Sobola.js
    Correction of the HNGi7

--- Gabriel-Sobola.js
+++ Gabriel-Sobola.js
@@ -1,10 +1,10 @@
 let Info = {
 
   Myname : "Sobola Gabriel Oluwatobi",
-  ID : "HNGi7-06269",
+  ID : "HNG-06269",
   Email : "sobolagabriel@gmail.com",
   Language : "javascript"
   
   };
 
-console.log(`Hello World, this is ${Info.Myname} with HNG ID ${Info.ID} and email ${Info.Email} using  ${Info.Language} for stage 2 task`)
+console.log(`Hello World, this is ${Info.Myname} with HNGi7 ID ${Info.ID} and email ${Info.Email} using  ${Info.Language} for stage 2 task`)
