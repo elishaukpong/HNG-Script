80c8c8dfb43ab21d9e7f656ce7c0f64c85fb14bc
Gabrinas <65027184+Gabrinas@users.noreply.github.com>
2020-06-07 14:47:10 +0100

    Create Gabriel-Sobola.js

---
+++ Gabriel-Sobola.js
@@ -0,0 +1,10 @@
+let Info = {
+
+  Myname : "Sobola Gabriel Oluwatobi",
+  ID : "HNGi7-06269",
+  Email : "sobolagabriel@gmail.com",
+  Language : "javascript"
+  
+  };
+
+console.log(`Hello World, this is ${Info.Myname} with HNG ID ${Info.ID} and email ${Info.Email} using  ${Info.Language} for stage 2 task`)
