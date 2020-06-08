eb475d09173fec0cd200e6b7e92ac858615d616c
Jasmel <66339078+jassytabs@users.noreply.github.com>
2020-06-08 13:14:12 +0200

    HNG-02231.php
    This is my presentation script for task 2. help me to know if it has merge please.

---
+++ scripts/HNG-02231.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World,this is Lepalack virgile jasmel with HNGi7 ID HNG-02231 using PHP for stage 2 task </p>'; ?>
+ </body>
+</html>
