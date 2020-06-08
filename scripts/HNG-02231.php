fde9aaef69412eda934ffd84a4f7a66256629bdb
Jasmel <66339078+jassytabs@users.noreply.github.com>
2020-06-08 13:39:58 +0200

    Update HNG-02231.php

--- scripts/HNG-02231.php
+++ scripts/HNG-02231.php
@@ -3,6 +3,6 @@
   <title>Test PHP</title>
  </head>
  <body>
- <?php echo '<p>Hello World,this is Lepalack virgile jasmel with HNGi7 ID HNG-02231 using PHP for stage 2 task </p>'; ?>
+ <?php echo '<p>Hello World,this is Lepalack virgile jasmel with HNGi7 ID HNG-02231 and email lepalackvirgile@gmail.com using PHP for stage 2 task </p>'; ?>
  </body>
 </html>
